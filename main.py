c7ddc7d816aff5d024d1e96b76cb6d813d573b54
coriegulik <coriegulik@gmail.com>
2022-11-25 15:05:08 -0800

    final

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from tkinter import *
 from tkinter import ttk
-import math
+from PIL import ImageTk, Image
+from urllib.request import urlopen
+import requests
+from io import BytesIO
 
+# home window
 root = Tk()
 root.title("Dogs4U")
-root.geometry("500x300")
+root.geometry("400x300")
 
 root.iconbitmap('c:/users/corie/pycharmprojects/Dogs4U/2.ico')
 label_title = Label(root, text="Dogs4U", font=16)
@@ -14,49 +18,8 @@ intro_1 = "\nWelcome to Dogs4U! \n\nSpecify any of the dog attributes and we wil
 label_directions = Label(root, text=intro_1)
 label_directions.grid(row=1, column=1, columnspan=6, rowspan = 5)
 
-# result logic
-def submitclick():
-    attrib_1 = drop_1.get()
-    attrib_2 = drop_2.get()
-    global result
-    global result_label
-    if attrib_1 == "No preference":
-        list_1 = sum(list(size_dict.values()), [])
-        if attrib_2 == "No preference":
-            list_2 = sum(list(activity_dict.values()), [])
-        else:
-            list_2 = activity_dict.get(attrib_2)
-    elif attrib_2 == "No preference":
-        list_2 = sum(list(activity_dict.values()), [])
-        list_1 = size_dict.get(attrib_1)
-    else:
-        list_1 = size_dict.get(attrib_1)
-        list_2 = activity_dict.get(attrib_2)
-    result_list = [value for value in list_1 if value in list_2]
-    if len(result_list) < 1:
-        result = "There are no dogs that match all the attributes"
-    else:
-        result = result_list[0]
-        for i in range(1, len(result_list)):
-            result += "\n" + result_list[i]
 
-# result window
-    top = Toplevel()
-    top.title("Dogs4U Result")
-    top.iconbitmap('c:/users/corie/pycharmprojects/Dogs4U/2.ico')
-    w = 500
-    h = max(300,math.ceil(len(result_list)/4)*100)
-    top.geometry(f"{w}x{h}")
-    intro2 = "Based on the selected attributes, below is the recommended dog 4 U!"
-    label_result = Label(top, text=intro2,)
-    label_result.grid(row=0, column=1, padx=10)
-    result_label = Label(top, text=result)
-    result_label.grid(row=1, column=1,padx=10, pady=50)
-    close_result = Button(top, text="Try Again", command=top.destroy)
-    close_result.grid(row=2,column=1)
-
-
-# drop down option labels
+# drop down option labels for home window
 dog_attrib_1 = "Average adult dog weight"
 label_directions = Label(root, text=dog_attrib_1)
 label_directions.grid(row=6, column=1, pady=10)
@@ -65,9 +28,43 @@ dog_attrib_2 = "Average adult dog energy level"
 label_directions = Label(root, text=dog_attrib_2)
 label_directions.grid(row=7, column=1, pady=10)
 
-blank_space = Label(root, text="")
-blank_space.grid(row=8, column=1, padx=10)
+dog_attrib_3 = "Average adult dog noise level"
+label_directions = Label(root, text=dog_attrib_3)
+label_directions.grid(row=8, column=1, pady=10)
 
+
+# makes list of dog breeds based on options chosen
+def submitclick():
+
+    global result_list  # list of dogs recommended
+
+    # gathers user inputs
+    attrib_1 = drop_1.get()
+    attrib_2 = drop_2.get()
+    attrib_3 = drop_3.get()
+
+    # making result_list of dog breeds matching attributes
+    if attrib_1 == "No preference":
+        list_1 = sum(list(size_dict.values()), [])
+    else:
+        list_1 = size_dict.get(attrib_1)
+    if attrib_2 == "No preference":
+        list_2 = list_1
+    else:
+        list_2 = list(set(list_1).intersection(activity_dict.get(attrib_2)))
+    if attrib_3 == "No preference":
+        result_list = list_2
+    else:
+        result_list = list(set(list_2).intersection(noise_dict.get(attrib_3)))
+
+    # opening window for results
+    if len(result_list) < 1:
+        exit_window()
+    else:
+        result_window()
+
+
+# submit
 submit_button = Button(root, text="Submit", padx=20, pady=10, command = submitclick)
 submit_button.grid(row=9, column=2)
 
@@ -94,59 +91,244 @@ drop_2 = ttk.Combobox(root, value=options_2)
 drop_2.current(0)
 drop_2.grid(row=7, column =2,pady=10)
 
-# dictionary of dog breeds
+drop_3 = ttk.Combobox(root, value=options_2)
+drop_3.current(0)
+drop_3.grid(row=8, column =2,pady=10)
+
+# dictionary of dog breeds based on size
 size_dict = {
     "<40 lbs":
-        ["French Bulldogs",
-        "Beagles",
-        "Dachshunds",
-        "Corgis",
-        "Yorkshire Terriers",
-        "Cavalier King Charles Spaniels",
-        "Miniature Schnauzers"],
+        ["French Bulldog",
+        "Beagle",
+        "Dachshund",
+        "Corgi",
+        "Yorkshire Terrier",
+        "Cavalier King Charles Spaniel",
+        "Miniature Schnauzer"],
     "40-80 lbs":
-        ["Labrador Retrievers",
-        "Golden Retrievers",
-        "Poodles",
-        "Bulldogs",
-        "German Shorthaired Pointers",
-        "Australian Shepherds",
-        "Boxers",
-        "Siberian Huskies"],
+        ["Labrador Retriever",
+        "Golden Retriever",
+        "Poodle",
+        "Bulldog",
+        "German Shorthaired Pointer",
+        "Australian Shepherd",
+        "Boxer",
+        "Siberian Husky"],
     ">80 lbs":
-        ["German Shepherds",
-        "Rottweilers",
-        "Doberman Pinschers",
-        "Great Danes"]
+        ["German Shepherd",
+        "Rottweiler",
+        "Doberman Pinscher",
+        "Great Dane"]
     }
 
+# dictionary of dog breeds based on activity
 activity_dict = {
     "Low":
-        ["French Bulldogs",
-        "Golden Retrievers",
-        "Bulldogs",
-        "Rottweilers",
-        "Dachshunds",
-        "Cavalier King Charles Spaniels",
-        "Miniature Schnauzers"],
+        ["French Bulldog",
+        "Golden Retriever",
+        "Bulldog",
+        "Rottweiler",
+        "Dachshund",
+        "Cavalier King Charles Spaniel",
+        "Miniature Schnauzer"],
     "Medium":
-        ["Poodles",
-        "Beagles",
-        "Corgis",
-        "Yorkshire Terriers",
-        "Boxers",
-        "Great Danes",
-        "Bernese Mountain Dogs"],
+        ["Poodle",
+        "Beagle",
+        "Corgi",
+        "Yorkshire Terrier",
+        "Boxer",
+        "Great Dane",
+        "Bernese Mountain Dog"],
     "High":
-        ["Labrador Retrievers",
-        "German Shepherds",
-        "German Shorthaired Pointers"
-        "Australian Shepherds",
-        "Doberman Pinschers",
-        "Siberian Huskies"]
+        ["Labrador Retriever",
+        "German Shepherd",
+        "German Shorthaired Pointer"
+        "Australian Shepherd",
+        "Doberman Pinscher",
+        "Siberian Husky"]
     }
 
+# dictionary of dog breeds based on noise
+noise_dict = {
+    "Low":
+        ["French Bulldog",
+        "Golden Retriever",
+        "Bulldog",
+        "Rottweiler",
+        ],
+    "Medium":
+        ["Labrador Retriever",
+        "German Shepherd",
+        "German Shorthaired Pointer"
+        "Australian Shepherd",
+        "Yorkshire Terrier",
+        "Boxer",
+        "Cavalier King Charles Spaniel",
+        "Doberman Pinscher",
+        "Great Dane",
+        "Bernese Mountain Dog"],
+    "High":
+        ["Poodle",
+        "Beagle",
+        "Corgi",
+        "Dachshund",
+        "Miniature Schnauzer",
+        "Siberian Husky"]
+    }
+# dictionary of dog breed to url ending
+url_dict = {
+    "Labrador Retriever": "labrador_retriever",
+    "French Bulldog": "french_bulldog",
+    "Golden Retriever":	"golden_retriever",
+    "German Shepherd": "german_shepherd",
+    "Poodle": "poodle",
+    "Bulldog": "bulldog",
+    "Beagle": "beagle",
+    "Rottweiler": "rotweiler",
+    "German Shorthaired Pointer": "german_shorthaired_pointer",
+    "Dachshund": "dachshund",
+    "Corgi": "pembroke_welsh_corgi",
+    "Australian Shepherd": "australian_shepherd",
+    "Yorkshire Terrier": "yorkshire_terrier",
+    "Boxer": "boxer",
+    "Cavalier King Charles Spaniel": "cavalier_king_charles_spaniel",
+    "Doberman Pinscher": "doberman_pinscher",
+    "Great Dane": "great_dane",
+    "Miniature Schnauzer":	"miniature_schnauzer",
+    "Siberian Husky": "siberian_husky",
+    "Bernese Mountain":	"bernese_mountain"
+}
 
+
+
+
+
+
+
+# result window with no matching dogs
+def exit_window():
+    top = Toplevel()
+    top.title("Dogs4U Result")
+    top.iconbitmap('c:/users/corie/pycharmprojects/Dogs4U/2.ico')
+    top.geometry("400x300")
+    intro2 = "Based on the selected attributes, below is the recommended dog 4 U!"
+    label_result1 = Label(top, text=intro2,)
+    label_result1.grid(row=0, column=0, columnspan=3)
+    result = "There are no dogs that match all the attributes"
+    label_result2 = Label(top, text=result)
+    label_result2.grid(row=2, column=1)
+    label_close = Button(top, text="Try Again", command=top.destroy)
+    label_close.grid(row=3,column=1)
+
+
+
+
+
+
+
+# microservice url
+restAPI = "http://natenate60.xyz:4004/"
+
+
+# result window
+def result_window():
+    global top, label_photo, image_num, result, label_result2, result_list
+
+    top = Toplevel()
+    top.title("Dogs4U Result")
+    top.iconbitmap('c:/users/corie/pycharmprojects/Dogs4U/2.ico')
+    top.geometry("400x300")
+    intro2 = "Based on the selected attributes, below is the recommended dog 4 U!"
+    label_result1 = Label(top, text=intro2,)
+    label_result1.grid(row=0, column=0, columnspan=3)
+
+    # dog breed image
+    image_num = 0
+    result = result_list[image_num]
+    response = restAPI+url_dict.get(result_list[image_num])
+    get_image(response)                                         # calls function that interacts with image microservice
+    label_photo = Label(top, image=photo)
+    label_photo.grid(row=1, column=0, columnspan=3)
+    label_result2 = Label(top, text=result)
+    label_result2.grid(row=2, column=1)
+
+    # buttons to scroll
+    label_close = Button(top, text="Try Again", command=top.destroy)
+    label_close.grid(row=3,column=1)
+    label_next = Button(top, text=">>", command=lambda: forward(image_num+1))
+    label_next.grid(row=3,column=2)
+    label_previous = Button(top, text="<<", command=lambda: back(image_num-1))
+    label_previous.grid(row=3,column=0)
+
+
+def forward(next):
+    global label_photo, label_next, response, image_num, label_result2, result_list
+
+    if next >= len(result_list):
+        label_next = Button(top, text=">>", state=DISABLED)
+    else:
+        image_num= next
+        label_photo.grid_forget()
+        label_result2.grid_forget()
+        result = result_list[image_num]
+        label_result2 = Label(top, text=result)
+        label_result2.grid(row=2, column=1)
+        response = restAPI+url_dict.get(result_list[image_num])
+        get_image(response)
+        label_photo = Label(top, image=photo)
+        label_photo.grid(row=1, column=0, columnspan=3)
+
+
+def back(previous):
+    global label_photo, label_previous, response, image_num, label_result2, result_list
+
+    if previous < 0:
+        label_previous = Button(top, text="<<", state=DISABLED)
+    else:
+        image_num= previous
+        label_photo.grid_forget()
+        label_result2.grid_forget()
+        result = result_list[image_num]
+        label_result2 = Label(top, text=result)
+        label_result2.grid(row=2, column=1)
+        response = restAPI+url_dict.get(result_list[image_num])
+        get_image(response)
+        label_photo = Label(top, image=photo)
+        label_photo.grid(row=1, column=0, columnspan=3)
+
+
+def resize(photo_p):
+    global width2, length2
+
+    resize_w = 350
+    resize_l = 210
+    resize_r = 5/3
+    a_ratio = photo_p.width()/photo_p.height()
+
+    if a_ratio > resize_r:
+        width2 = int(resize_w)
+        length2 = int(resize_w/a_ratio)
+    else:
+        length2 = int(resize_l)
+        width2 = int(resize_l*a_ratio)
+
+
+def get_image(url):
+    # calls on image microservice REST API to get link to image
+
+    print("Getting image from " + url)
+    global photo
+
+    response = requests.get(url).text
+    u = urlopen(response)
+    raw_data = u.read()
+    u.close()
+    photo_p = ImageTk.PhotoImage(data=raw_data)
+    resize(photo_p)
+    r = requests.get(response)
+    pilImage = Image.open(BytesIO(r.content))
+    resized = pilImage.resize((width2,length2),Image.ANTIALIAS)
+    photo = ImageTk.PhotoImage(resized)
 
 
 root.mainloop()
